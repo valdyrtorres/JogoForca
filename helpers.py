a5d220d754aa2a98b1f9175fa3a8403a1ed7cb08
Valdir Torres Borges <valdyr_cloud01@outlook.com>
2022-08-04 10:21:56 -0300

    Criada as funções verificar_letra_informada e total_tentativas

--- helpers.py
+++ helpers.py
@@ -4,9 +4,43 @@ from config import TENTATIVAS_ADICIONAIS, ARQUIVO_PALAVRAS_SECRETAS
 def gerar_palavra_secreta():
     """
     Função para randomicamente selecionar uma palavra do arquivo texto
-    :return:
+    :return: uma palavra aleatória
     """
     with open(ARQUIVO_PALAVRAS_SECRETAS, 'r') as f_obj:
         palavra = f_obj.read().splitlines()
 
     return random.choice(palavra)
+
+
+def verificar_letra_informada(palavra_secreta, suas_tentativas, tentativa):
+    """
+    Verifica se a letra dada está correta
+    :param palavra_secreta: Gerada com base no arquivo texto de palavras secretas
+    :param suas_tentativas: Lista com todas as tentativas
+    :param tentativa: letra inserida nesta jogada
+    :return: retorna um status
+    """
+    status = '' # O status precisa ser zerado toda vez que a função for chamada
+    acertos = 0 # Também precisa ser zerado para essa jogada/tentativa
+
+    for letra in palavra_secreta:
+        if letra.lower() in suas_tentativas:
+            status += letra
+        else:
+            status += '*'
+
+        if letra.lower() == tentativa.lower():
+            acertos += 1
+
+    print(f"\n - Acertos {acertos} letras(s) '{tentativa}")
+
+    return status
+
+def total_tentativas(palavra_secreta):
+    """
+    Define a quantidade de tentativas de acordo com a palavra secreta
+    :param palavra_secreta: gerada aleatoriamente
+    :return: Quantidade de tentativas
+    """
+    chances = len(palavra_secreta)
+    return chances + TENTATIVAS_ADICIONAIS
